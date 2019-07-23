5c3b2efb45478c07c12518f7184a94580aa0865e
dase261566 <dan.secareanu@elektrobit.com>
2019-07-23 10:04:12 +0300

    Initial commit.

---
+++ bot.js
@@ -0,0 +1,65 @@
+var Discord = require('discord.js');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Initialize Discord Bot
+var bot = new Discord.Client();
+bot.login("NjAzMDk1ODUxMTk1NDMyOTYx.XTabbA.gFO3GwbXki_oCygiPN5v-1Twkp8");
+
+bot.on("ready", () => {
+ console.log("I am ready!");
+ var really = bot.emojis.find(emoji => emoji.name === "reallynigga");
+});
+
+bot.on("message", (message) => {
+    if (message.content.startsWith('!')) {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'dice':
+            {
+                if(args[0] == undefined) {
+                    message.channel.send("Returns a random number between 0 and the number given.");
+                    break;
+                }
+                var max = parseInt(args[0],10);
+                if(isNaN(max)) {
+                    var really = bot.emojis.find(emoji => emoji.name === "reallynigga");
+                    message.channel.send(really.toString() +" Bruh, that ain't a number.");
+                    break;
+                }
+                var number = getRandomInt(max);
+                var nice = "";
+                if(number==69)
+                    nice = "(nice)";
+                message.channel.send('Your number: '+getRandomInt(max)+nice);
+            }
+            break;
+            case 'status':
+            {
+                const { exec } = require('child_process');
+                exec('mcstatus dochia.go.ro:25565 status', (err, stdout, stderr) => {
+                //`${stderr}`
+                if(err){
+                    var error_str = `${stderr}`;
+                    error_str = error_str.trim();
+                    if(error_str.endsWith("timed out")){
+                        message.channel.send("Server offline.");
+                    } else {
+                        message.channel.send("Something went wrong...");
+                    }
+                } else {
+                    message.channel.send(`${stdout}`);
+                }
+                
+                });
+            }    
+            break;
+        }
+     }
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+To install this bastard:
+
+* install nodejs
+* run "npm install"
+* run "node bot.js" 
