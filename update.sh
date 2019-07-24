c63dea96bc69f821fb7eabac71be2c857cc8ee99
Bluegent <bluegentleman@live.com>
2019-07-24 21:21:39 +0300

    Added some log messages to update script.

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo " #================================#"
+echo "#================================#"
 killall node
 sleep 1
+echo "Attempting git pull..."
 git pull
+echo "Git pull end."
 sleep 1
 nohup node bot.js &
-echo " #================================#"
