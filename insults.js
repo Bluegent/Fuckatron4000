c812a9efeada03efc572deb1c699e49983ec5efe
Bluegent <bluegentleman@live.com>
2019-10-14 21:52:11 +0300

    More insult tweaks

--- insults.js
+++ insults.js
@@ -8,19 +8,23 @@ var locationT = "@location";
 var tagT= ">>tag";
 var adjectiveT = "@adjective";
 var associateT = "@assoc";
+var containerT = "@contanier";
 
-
-var nounArr = ["fart","cum","shit","dick","anus"];
-var verbArr = ["smell","drink","slurp","gargle","love","sniff","suck","steal"];
-var locationArr = ["in hell","in their mother's basement","in a "+ adjectiveT+" bar","behind the Kaufland store","in their boss' office"];
-var associateArr = ["goblin","associate","enthusiast","thief","bastard"];
-var adjectiveArr = ["crusty","smelly","nasty","decrepit","ancient","old","deadly","dank"];
+var nounArr = ["fart","cum","shit","dick","anus","penis","piss","poop","turd","crap","ballsack","puke","jizz", "period blood","fetus","diaper","ball"];
+var verbArr = ["smell","drink","slurp","gargle","love","sniff","suck","steal","smuggle","kiss","munch","eat","swallow","feel","fondle","touch","hold","sell","lick"];
+var locationArr = ["in hell","in their mother's basement","in a crusty bar","behind the Kaufland store","in their boss' office"];
+var associateArr = ["goblin","associate","enthusiast","thief","bastard","asshole","midget","dwarf","zombie","monster","dweller","hobo","fag","admirer","whore","fan","rapist","merchant"];
+var adjectiveArr = ["crusty","smelly","nasty","decrepit","ancient","old","deadly","dank","gay","hairy","crispy","flaccid","musty","crummy","sweaty"];
+var containerArr = ["tub","bucket","box","room"];
 var insultTemplates = [];
 
 exports.initTemplates = function(){
     insultTemplates.push(tagT+" is a "+nounT+" "+gerundT+"-"+nounT+" "+doerT+".");
-    insultTemplates.push(tagT+" "+verbT+"s " +nounT+" "+ locationT+".");
+    insultTemplates.push(tagT+" is a "+nounT+" "+gerundT+"-"+nounT+" "+associateT+".");
+    insultTemplates.push(tagT+" "+verbT+" " +nounT+" "+ locationT+".");
+    insultTemplates.push(tagT+" "+verbT+" " +associateT+" "+nounT+".");
     insultTemplates.push(tagT+" is a "+adjectiveT+" "+nounT+" "+associateT+".");
+    insultTemplates.push(tagT+" lives in a "+containerT+" filled with "+nounT+".");
 }
 
 function ran(max) {
@@ -35,7 +39,7 @@ function endsWithVowel(string){
     return lower.endsWith("a") || lower.endsWith("e") || lower.endsWith("i") || lower.endsWith("o")   || lower.endsWith("u");
 }
 
-function removeLastVowel(string){
+function removeLastLetter(string){
     if(endsWithVowel(string)){
         return string.substring(0,string.length-1);
     } else {
@@ -44,15 +48,20 @@ function removeLastVowel(string){
 }
 
 function getRandomGerund(){
-    return removeLastVowel(ranS(verbArr))+"ing";
+    return removeLastLetter(ranS(verbArr))+"ing";
 }
 
 function getRandomDoer(){
-    return removeLastVowel(ranS(verbArr))+"er";
+    return removeLastLetter(ranS(verbArr))+"er";
 }
 
 function getRandomVerb(){
-    return ranS(verbArr);
+    var verb = ranS(verbArr);
+    var lower = verb.toLowerCase();
+    if(lower.endsWith("s")){
+        return verb+"es";
+    }
+    return verb+"s";
 }
 function getRandomNoun(){
     return ranS(nounArr);
@@ -76,6 +85,7 @@ function replaceTags(string){
     copy = copy.replace(doerT,getRandomDoer());
     copy = copy.replace(gerundT,getRandomGerund());
     copy = copy.replace(associateT,getRandomAssociate());
+    copy = copy.replace(containerT,ranS(containerArr));
     return copy
 }
 
